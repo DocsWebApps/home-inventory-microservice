8d9a7e5300ef2853f4d0424114213f0b53b83e31
Dave Collier <lardelbow@gmail.com>
2019-12-01 20:12:55 +0000

    Spring boot 2.2.1 upgrade

--- src/main/java/com/docswebapps/homeinventoryservice/config/WebSecurityConfig.java
+++ src/main/java/com/docswebapps/homeinventoryservice/config/WebSecurityConfig.java
@@ -1,24 +1,19 @@
 package com.docswebapps.homeinventoryservice.config;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Value("${USERNAME}")
-    private String username;
-
-    @Value("${PASSWORD}")
-    private String password;
+//    @Value("${USERNAME}")
+//    private String username;
+//
+//    @Value("${PASSWORD}")
+//    private String password;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,18 +26,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .and().cors()
         .and().csrf().disable();
     }
-
-    @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        UserDetails admin =
-                User
-                        .withDefaultPasswordEncoder()
-                        .username(this.username)
-                        .password(this.password)
-                        .roles("ADMIN")
-                        .build();
-        return new InMemoryUserDetailsManager((admin));
-    }
+//
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails admin =
+//                User
+//                        .withDefaultPasswordEncoder()
+//                        .username(this.username)
+//                        .password(this.password)
+//                        .roles("ADMIN")
+//                        .build();
+//        return new InMemoryUserDetailsManager((admin));
+//    }
 }
 
--- src/main/java/com/docswebapps/homeinventoryservice/web/controller/v1/MakeController.java
+++ src/main/java/com/docswebapps/homeinventoryservice/web/controller/v1/MakeController.java
@@ -1,32 +1,15 @@
 package com.docswebapps.homeinventoryservice.web.controller.v1;
 
 import com.docswebapps.homeinventoryservice.web.model.MakeDto;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
 
 @RestController
 @RequestMapping("/api/v1/make")
 public class MakeController {
-
-    @Value("${INSTANCE_ID}")
-    private String instanceId;
-
-    @GetMapping("/test")
-    public ResponseEntity<MakeDto> getTestDTO() {
-        MakeDto makeDto = MakeDto.builder()
-                .id(0L)
-                .name(this.instanceId)
-                .createdDate(OffsetDateTime.now(ZoneId.systemDefault()))
-                .lastModifiedDate(OffsetDateTime.now(ZoneId.systemDefault()))
-                .build();
-        return ResponseEntity.ok().body(makeDto);
-    }
 
     @GetMapping("/{makeId}")
     public ResponseEntity<MakeDto> getMakeById(@PathVariable("makeId") Long makeId) {
